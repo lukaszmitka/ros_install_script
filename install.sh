f7e5f5b703d29968f30024e42d87fd28462001a2
Łukasz Mitka <luk.mitka@gmail.com>
2016-12-13 17:16:48 +0100

    Add installation of required packets

--- install.sh
+++ install.sh
@@ -2,7 +2,9 @@
 echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list
 apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 0xB01FA116
 apt-get update
-apt-get --fix-missing install -y ros-jade-desktop-full 
+apt get -y upgrade
+apt-get -y install ubuntu-desktop gnome libssl0.9.8
+apt-get --fix-missing install -y ros-jade-desktop-full libopencv-dev ros-jade-gmapping ros-jade-turtle-tf ros-jade-navigation ros-jade-find-object-2d ros-jade-frontier-exploration
 
 rosdep init
 su student -c 'rosdep update'
@@ -12,8 +14,29 @@ echo "source /opt/ros/jade/setup.bash" >> /etc/bash.bashrc
 echo 'export ROS_IP=$(ifconfig eth | sed -n '\''s/.*inet addr:\([0-9.]\+\)\s.*/\1/p'\'')' >> /etc/bash.bashrc
 echo 'export ROS_MASTER_URI=http://$(ifconfig eth | sed -n '\''s/.*inet addr:\([0-9.]\+\)\s.*/\1/p'\''):11311' >> /etc/bash.bashrc
 
+rospack find gmapping
+rospack find amcl
+rospack find carrot_planner
+rospack find dwa_local_planner
+rospack find move_base
+rospack find nav_core
+rospack find robot_pose_ekf
+rospack find base_local_planner
+rospack find clear_costmap_recovery
+rospack find fake_localization
+rospack find global_planner
+rospack find move_base_msgs
+rospack find navfn
+rospack find rotate_recovery
+rospack find costmap_2d
+rospack find map_server
+rospack find move_slow_and_clear
+rospack find voxel_grid
+rospack find find_object_2d
+rospack find usb_cam
+rospack find frontier_exploration
 
-wget http://coppeliarobotics.com/V-REP_PRO_EDU_V3_3_2_64_Linux.tar.gz
+wget wget http://coppeliarobotics.com/files/V-REP_PRO_EDU_V3_3_2_64_Linux.tar.gz
 tar -zxvf V-REP_PRO_EDU_V3_3_2_64_Linux.tar.gz
 
 rm -rf /usr/share/vrep
